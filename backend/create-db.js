0ea4d7d4405780cd16b1090b040ddcc25193565a
Noam <63012023+noam-y@users.noreply.github.com>
2022-07-06 18:40:18 +0300

    added basic insert query, still doesnt use date from the form

--- backend/create-db.js
+++ backend/create-db.js
@@ -18,12 +18,15 @@ con.connect(function (err) {
 });
 
 
-var CREATE_COMPLAINTS_TABLE_QUERY = "CREATE TABLE visitors_complaints(id INT, date_received DATE, phone VARCHAR(20),email VARCHAR(25),taz VARCHAR(9), car_id varchar(9), date_commited DATE, details VARCHAR(400))";
-
+var CREATE_COMPLAINTS_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS visitors_complaints(id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, date_received DATE, phone VARCHAR(20),email VARCHAR(25),taz VARCHAR(9), car_id varchar(9) NOT NULL, date_commited DATE, details VARCHAR(400))";
+var NEW_VCOMPLAINT = "INSERT INTO visitors_complaints (date_received, phone,email, taz, car_id, date_commited, details) VALUES (STR_TO_DATE('22-01-2012', '%d-%m-%Y'), '0524657857', 'email@gmail.com', '475466545', '74754858', '21-7-2001', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')"
 con.query(CREATE_COMPLAINTS_TABLE_QUERY, function (err, result) {
   if (err) throw err;
   console.log("visitors_complaints table created");
 });
 
 
-
+con.query(NEW_VCOMPLAINT, function (err, result) {
+  if (err) throw err;
+  console.log("inset");
+});
