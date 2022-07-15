26233223c6ef19516d9218da061a709bbe157720
Noam <63012023+noam-y@users.noreply.github.com>
2022-07-15 13:45:15 +0300

    added router.param that gives us the current complainId we are working on. also formatted file using prettier

--- backend/db/create-db.js
+++ backend/db/create-db.js
@@ -3,7 +3,7 @@ var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "complaints"
+  database: "complaints",
 });
 con.connect(function (err) {
   // will only create complaints db if it doesnt exist.
@@ -13,37 +13,51 @@ con.connect(function (err) {
     if (err) throw err;
     console.log("Database exists/created");
   });
-  
-
 });
 
-
-var CREATE_COMPLAINTS_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS visitors_complaints(id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, date_received DATE, phone VARCHAR(20),email VARCHAR(25),taz VARCHAR(9), car_id varchar(9) NOT NULL, date_commited DATE, details VARCHAR(400))";
-var NEW_VCOMPLAINT = "INSERT INTO visitors_complaints (date_received, phone,email, taz, car_id, date_commited, details) VALUES (STR_TO_DATE('22-01-2012', '%d-%m-%Y'), '0524657857', 'email@gmail.com', '475466545', '74754858', '21-7-2001', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')"
-var NEW_VCOMPLAINT_F = "INSERT INTO visitors_complaints (date_received, phone,email, taz, car_id, date_commited, details) VALUES ?"
+var CREATE_COMPLAINTS_TABLE_QUERY =
+  "CREATE TABLE IF NOT EXISTS visitors_complaints(id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, date_received DATE, phone VARCHAR(20),email VARCHAR(25),taz VARCHAR(9), car_id varchar(9) NOT NULL, date_commited DATE, details VARCHAR(400))";
+var NEW_VCOMPLAINT =
+  "INSERT INTO visitors_complaints (date_received, phone,email, taz, car_id, date_commited, details) VALUES (STR_TO_DATE('22-01-2012', '%d-%m-%Y'), '0524657857', 'email@gmail.com', '475466545', '74754858', '21-7-2001', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')";
+var NEW_VCOMPLAINT_F =
+  "INSERT INTO visitors_complaints (date_received, phone,email, taz, car_id, date_commited, details) VALUES ?";
 var EXAMPLE_VALUES = [
-  ['22-01-2013', '098354675', 'email22@gmail.com', '432546945', '44664123', '02-12-1995', 'hello world'],
-  ['14-07-2000', '077354675', 'email2214@gmail.com', '45641555', '12312312', '16-10-1975', 'hi hi world']
-]
+  [
+    "22-01-2013",
+    "098354675",
+    "email22@gmail.com",
+    "432546945",
+    "44664123",
+    "02-12-1995",
+    "hello world",
+  ],
+  [
+    "14-07-2000",
+    "077354675",
+    "email2214@gmail.com",
+    "45641555",
+    "12312312",
+    "16-10-1975",
+    "hi hi world",
+  ],
+];
 con.query(CREATE_COMPLAINTS_TABLE_QUERY, function (err, result) {
   if (err) throw err;
   console.log("visitors_complaints table created");
 });
 
 single_insert = () => {
-con.query(NEW_VCOMPLAINT, function (err, result) {
-  if (err) throw err;
-  console.log("inset");
-});
-}
+  con.query(NEW_VCOMPLAINT, function (err, result) {
+    if (err) throw err;
+    console.log("inset");
+  });
+};
 
 mul_insert = () => {
   con.query(NEW_VCOMPLAINT_F, [EXAMPLE_VALUES], function (err, result) {
     if (err) throw err;
     console.log("inset");
   });
-  }
+};
 
-mul_insert()
-  
-
+mul_insert();
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,38 +1,62 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
+router.get("/new", (req, res) => {
+  res.render("users/new", { current_time: "wednesday" });
+});
 
-router.get('/new', (req,res) => {
-    res.render("users/new", {current_time:"wednesday"})
-})
+var complaints = [
+  [
+    "22-01-2013",
+    "098354675",
+    "email22@gmail.com",
+    "432546945",
+    "44664123",
+    "02-12-1995",
+    "hello world",
+  ],
+  [
+    "14-07-2000",
+    "077354675",
+    "email2214@gmail.com",
+    "45641555",
+    "12312312",
+    "16-10-1975",
+    "hi hi world",
+  ],
+];
 
-var users = [
-    ['22-01-2013', '098354675', 'email22@gmail.com', '432546945', '44664123', '02-12-1995', 'hello world'],
-    ['14-07-2000', '077354675', 'email2214@gmail.com', '45641555', '12312312', '16-10-1975', 'hi hi world']
-  ]
-  
-
-router.post('/', (req,res) => {
-    const isValid = true // in the future this will be used to validate user details
-    if (isValid){
-        users.push({
-            phone : req.body.phone,
-            email: req.body.email,
-            reported_car : req.body.reported_car
-        })
-        res.redirect(`/users/${users.length -1}`)
-    } else {
-        console.log('ERROR')
-        res.send(`heyyyyy, the request about laz failed`)
-
-    }
-})
+router.post("/", (req, res) => {
+  const isValid = true; // in the future this will be used to validate complaint details
+  if (isValid) {
+    complaints.push({
+      phone: req.body.phone,
+      email: req.body.email,
+      reported_car: req.body.reported_car,
+    });
+    res.redirect(`/users/${complaints.length - 1}`);
+    console.log(req.complaint);
+  } else {
+    console.log("ERROR");
+    res.send(`heyyyyy, the request about laz failed`);
+  }
+});
 
 router
-.get('/:complaintId', (req, res) => {
-    res.send(`getting all the details about complaint number ${req.params.complaintId}`)
-})
-.delete((req,res) => {
-    res.send(`deleting complaint number ${req.params.complaintId}`)
-})
-module.exports = router
+  .get("/:complaintId?", (req, res) => {
+    res.send(
+      `getting all the details about complaint number ${req.params.complaintId}- `
+    );
+  })
+  .delete((req, res) => {
+    res.send(`deleting complaint number ${req.params.id}`);
+  });
+
+router.param("complaintId", (req, res, next, id) => {
+  console.log("param middleware is called");
+  req.complaint = complaints[id];
+  console.log("current complaintId is", req.complaint);
+  next();
+});
+
+module.exports = router;
