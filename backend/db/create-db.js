d6d197096e6319934a4e41bc0c56f4be59cc566b
Noam <63012023+noam-y@users.noreply.github.com>
2022-07-25 12:28:45 +0300

    added the date the report was commited to mysql table

--- backend/db/create-db.js
+++ backend/db/create-db.js
@@ -5,6 +5,7 @@ var con = mysql.createConnection({
   password: "",
   database: "complaints",
 });
+
 con.connect(function (err) {
   // will only create complaints db if it doesnt exist.
   if (err) throw err;
@@ -16,11 +17,14 @@ con.connect(function (err) {
 });
 
 var CREATE_COMPLAINTS_TABLE_QUERY =
-  "CREATE TABLE IF NOT EXISTS visitors_complaints(id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, date_received DATE, phone VARCHAR(20),email VARCHAR(25),taz VARCHAR(9), car_id varchar(9) NOT NULL, date_commited DATE, details VARCHAR(400))";
+  "CREATE TABLE IF NOT EXISTS visitors_complaints(id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, date_received DATE, reporter_name VARCHAR(20), phone VARCHAR(20),email VARCHAR(25),taz VARCHAR(9), car_id varchar(9) NOT NULL, date_commited DATE, details VARCHAR(400))";
 var NEW_VCOMPLAINT =
-  "INSERT INTO visitors_complaints (date_received, phone,email, taz, car_id, date_commited, details) VALUES (STR_TO_DATE('22-01-2012', '%d-%m-%Y'), '0524657857', 'email@gmail.com', '475466545', '74754858', '21-7-2001', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')";
+  "INSERT INTO visitors_complaints (date_received, reporter_name, phone,email, taz, car_id, date_commited, details) VALUES (STR_TO_DATE('22-01-2012', '%d-%m-%Y'), '0524657857', 'nnnnneeewwwww@gmail.com', '475466545', '74754858', '21-7-2001', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')";
 var NEW_VCOMPLAINT_F =
-  "INSERT INTO visitors_complaints (date_received, phone,email, taz, car_id, date_commited, details) VALUES ?";
+  "INSERT INTO visitors_complaints (date_received, reporter_name, phone,email, taz, car_id, date_commited, details) VALUES ?";
+  var NEW_VCOMPLAINT_WO_DATES =
+  "INSERT INTO visitors_complaints (reporter_name, phone, email, taz, car_id, details, date_received) VALUES (?, current_date())";
+
 var EXAMPLE_VALUES = [
   [
     "22-01-2013",
@@ -46,18 +50,22 @@ con.query(CREATE_COMPLAINTS_TABLE_QUERY, function (err, result) {
   console.log("visitors_complaints table created");
 });
 
-single_insert = () => {
+singleInsert = () => {
   con.query(NEW_VCOMPLAINT, function (err, result) {
     if (err) throw err;
     console.log("inset");
   });
 };
 
-mul_insert = () => {
-  con.query(NEW_VCOMPLAINT_F, [EXAMPLE_VALUES], function (err, result) {
+mulInsert = (givenValues) => {
+  con.query(NEW_VCOMPLAINT_WO_DATES, [givenValues], function (err, result) {
     if (err) throw err;
     console.log("inset");
   });
-};
+}
 
-mul_insert();
+mulInsert
+module.exports = {
+  singleInsert,
+  mulInsert,
+};
