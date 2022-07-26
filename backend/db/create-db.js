17d3f778e4f4973a8fcb152f1ac9579c0a2ea34b
Noam <63012023+noam-y@users.noreply.github.com>
2022-07-26 11:55:44 +0300

    moved server out of middleware folder

--- backend/db/create-db.js
+++ backend/db/create-db.js
@@ -20,10 +20,8 @@ var CREATE_COMPLAINTS_TABLE_QUERY =
   "CREATE TABLE IF NOT EXISTS visitors_complaints(id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, date_received DATE, reporter_name VARCHAR(20), phone VARCHAR(20),email VARCHAR(25),taz VARCHAR(9), car_id varchar(9) NOT NULL, date_commited DATE, details VARCHAR(400))";
 var NEW_VCOMPLAINT =
   "INSERT INTO visitors_complaints (date_received, reporter_name, phone,email, taz, car_id, date_commited, details) VALUES (STR_TO_DATE('22-01-2012', '%d-%m-%Y'), '0524657857', 'nnnnneeewwwww@gmail.com', '475466545', '74754858', '21-7-2001', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')";
-var NEW_VCOMPLAINT_F =
-  "INSERT INTO visitors_complaints (date_received, reporter_name, phone,email, taz, car_id, date_commited, details) VALUES ?";
-  var NEW_VCOMPLAINT_WO_DATES =
-  "INSERT INTO visitors_complaints (reporter_name, phone, email, taz, car_id, details, date_received) VALUES (?, current_date())";
+var NEW_VCOMPLAINT_W_DATES =
+  "INSERT INTO visitors_complaints (reporter_name, phone, email, taz, car_id, details, date_commited, date_received) VALUES (?, current_date())";
 
 var EXAMPLE_VALUES = [
   [
@@ -58,13 +56,13 @@ singleInsert = () => {
 };
 
 mulInsert = (givenValues) => {
-  con.query(NEW_VCOMPLAINT_WO_DATES, [givenValues], function (err, result) {
+  con.query(NEW_VCOMPLAINT_W_DATES, [givenValues], function (err, result) {
     if (err) throw err;
     console.log("inset");
   });
-}
+};
 
-mulInsert
+mulInsert;
 module.exports = {
   singleInsert,
   mulInsert,
